623b198c0c6a32a6042c28d22da97bc07f80ed2c
OliveKodo <tomergal40@gmail.com>
2025-04-22 15:01:30 +0300

    firt commit

--- SquareMat.cpp
+++ SquareMat.cpp
@@ -1,3 +1,16 @@
 // tomergal40@gmail.com
 #include "SquareMat.hpp"
 #include <stdexcept>
+
+namespace Matrix {
+
+// Constructor with size - creates an empty matrix filled with zeros
+SquareMat::SquareMat(int size) {
+        if (size <= 0) {
+            throw std::invalid_argument("Matrix size must be positive");
+        }
+        
+    }  
+
+
+}
